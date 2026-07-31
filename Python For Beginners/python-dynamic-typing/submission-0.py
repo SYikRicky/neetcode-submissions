c07a4778d70cf9be9309f4ed1b99522207679117
Rickyyysin <152773505+SYikRicky@users.noreply.github.com>
2026-07-31 21:21:58 +0800

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,11 @@
+variable = 1
+print(type(variable))
+variable = 1.0
+print(type(variable))
+variable = True
+
+print(type(variable))
+variable = "hk"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
