a5e5d0c4ba18cf7c4d87c7a6eb0db67b4717331c
Rickyyysin <152773505+SYikRicky@users.noreply.github.com>
2026-08-30 15:21:31 +0800

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        res = 0
+        time_stack = []
+        pos_speed = [list(item) for item in zip(position, speed)]
+        pos_speed.sort(key=lambda pos: pos[0], reverse=True)
+        for idx, pair in enumerate(pos_speed):
+            time = (target - pair[0]) / pair[1]
+            if time_stack and time_stack[-1] >= time:
+                continue
+            time_stack.append(time)
+            res += 1
+        return res
